dd5a44b08b59a51c6565693367a60fed70bf26dd
Ashish Chaubey <ashishchaubey866@gmail.com>
2024-07-24 01:37:22 +0530

    feat: add chakra-ui and react-router-dom library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = () => {
-  return <div>Hillo</div>;
+  return (
+    <Router>
+      <ChakraProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </ChakraProvider>
+    </Router>
+  );
 };
 
 export default App;
