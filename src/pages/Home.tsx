340f4d80b87977c171f5ea7ec4e00fc250486e55
Ashish Chaubey <ashishchaubey866@gmail.com>
2024-07-27 21:54:51 +0530

    fix: remove commenst

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,6 @@ import { useQuery } from "@apollo/client";
 import { GET_DATA } from "../queries/fetchData";
 
 export default function Home() {
-  // const { loading, error, data } = useQuery(GET_DATA);
-
-  // // Update UI later if time permits
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error: {error.message}</p>;
-
   return (
     <div>
       <Navbar />
