070849e776b1115ac9733e09b21d07c47987feaa
Ashish Chaubey <ashishchaubey866@gmail.com>
2024-07-27 21:55:10 +0530

    fix: remove unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Library from "./components/Library";
 import Navbar from "./components/Navbar";
-import { useQuery } from "@apollo/client";
-import { GET_DATA } from "../queries/fetchData";
 
 export default function Home() {
   return (
